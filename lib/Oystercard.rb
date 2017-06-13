26654c02f5b802a038c9e005e4f27df2ab36a652
tbscanlon <thomas.scanlon@outlook.com>
2017-06-13 16:14:58 +0100

    Add record of journeys

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,19 +1,19 @@
 class Oystercard
 
-attr_reader :balance, :status, :entry_station
+  attr_reader :balance, :entry_station, :previous_journeys
 
   MAX_LIMIT = 100
   MIN_FARE = 1
 
-  def initialize(balance = 0, status = :not_in_use)
-    @balance = balance
-    @status = status
-    @entry_station = nil
+  def initialize(balance = 0)
+    @balance           = balance
+    @entry_station     = nil
+    @previous_journeys = []
   end
 
   def topup(amount)
-    fail "You have reached your maximum balance limit" if @balance+amount > MAX_LIMIT
-    @balance+= amount
+    raise "You have reached your maximum balance limit" if @balance + amount > MAX_LIMIT
+    @balance += amount
   end
 
   def in_journey?
@@ -23,22 +23,21 @@ attr_reader :balance, :status, :entry_station
   def touch_in(station)
     raise "Please top up" if @balance < MIN_FARE
     @entry_station = station
-    @status = :in_use
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MIN_FARE)
+    record_journey(entry_station, station)
     @entry_station = nil
-    @status = :not_in_use
   end
 
-
-
-private
-
+  private
   def deduct(amount)
     @balance-= amount
   end
 
-
+  def record_journey(entry_station, exit_station)
+    journey = { entry: entry_station, exit: exit_station }
+    @previous_journeys.push journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,12 +3,15 @@ require 'Oystercard'
 describe Oystercard do
 
   let(:station) { double(:kings_cross) }
+  let(:exit_station) { double(:waterloo) }
 
   it { is_expected.to respond_to(:topup).with(1).argument }
   # it { is_expected.to respond_to(:deduct).with(1).argument }
   it { is_expected.to respond_to(:touch_in).with(1).argument }
-  it { is_expected.to respond_to(:touch_out) }
+  it { is_expected.to respond_to(:touch_out).with(1).argument }
   it { is_expected.to respond_to(:entry_station) }
+  # it { is_expected.to respond_to(:exit_station) }
+  it { is_expected.to respond_to(:previous_journeys)}
 
   describe "#balance" do
     it "should return the balance of the card" do
@@ -64,25 +67,28 @@ describe Oystercard do
   end
 
   describe "#touch_out" do
-    it "should change in_journey to false" do
+    before do
       subject.topup(Oystercard::MIN_FARE)
       subject.touch_in(station)
-      subject.touch_out
+      subject.touch_out(exit_station)
+    end
+
+    it "should change in_journey to false" do
       expect(subject).not_to be_in_journey
     end
 
+    it "records the journey" do
+      expect(subject.previous_journeys.last).to eq ({entry: station, exit: exit_station})
+    end
+
     it "should set entry_station to nil" do
-      subject.topup(Oystercard::MIN_FARE)
-      subject.touch_in(station)
-      subject.touch_out
       expect(subject.entry_station).to be_nil
     end
 
     it "should deduct my balance by the fare amount" do
-      subject.topup(Oystercard::MIN_FARE)
-      subject.touch_in(station)
-      expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MIN_FARE
+      expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by -Oystercard::MIN_FARE
     end
+
   end
 
 end
